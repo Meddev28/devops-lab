3dc336c2b7179a31c529f1fbf7c22318152b0fe7
Meddev28 <hidamohammed19@gmail.com>
2026-04-24 10:12:05 +0100

    push test

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "version 6 - deployement automatic test5"
+    return "version 2 - deployement automatic test5"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
